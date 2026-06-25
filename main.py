90af48830624598f916f2afd3008551efb50aeb6
Guriqbal Manyani <gmanyani527@gmail.com>
2026-06-25 02:06:55 +0000

    Merge Ticketmaster API with Gemini event search

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import json
+import requests
+from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from pydantic import BaseModel, Field
 
-
+load_dotenv()
 class TicketmasterParams(BaseModel):
     keyword: list[str]
     classificationName: str
@@ -22,6 +24,21 @@ class EventSearch(BaseModel):
     ticketmaster: TicketmasterParams
     google_events: GoogleEventsParams
 
+def get_events(keyword,city):
+  api_key = os.getenv("TICKETMASTER_API_KEY")
+  url = "https://app.ticketmaster.com/discovery/v2/events.json"
+
+  params = {
+    "apikey": api_key,
+    "keyword": keyword,
+    "city": city,
+    "size": 5
+  }
+  response = requests.get(url, params=params)
+  if response.status_code == 200:
+    return response.json()
+  print("Error:", response.status_code)
+  return None
 
 genai_key = os.getenv('GENAI_KEY')
 genai.api_key = genai_key
@@ -47,3 +64,20 @@ search_terms = json.loads(events)
 print(f"Ticketmaster Keyword: {search_terms['ticketmaster']['keyword']}")
 print(f"Ticketmaster City: {search_terms['ticketmaster']['city']}")
 print(f"Google Events Query: {search_terms['google_events']['q']}")
+
+ticketmaster_keywords = search_terms["ticketmaster"]["keyword"]
+ticketmaster_city = search_terms["ticketmaster"]["city"]
+
+ticketmaster_results = get_events(
+  ticketmaster_keywords[0],
+  ticketmaster_city
+)
+if ticketmaster_results and "_embedded" in ticketmaster_results:
+  for event in ticketmaster_results["_embedded"]["events"]:
+    print(f"Name: {event['name']}")
+    print(f"Date: {event['dates']['start'].get('localDate', 'N/A')}")
+    print(f"Venue: {event[_embedded]['venues'][0]['names']}")
+    print(f"City: {event['_embedded']['venues'][0]['city']['name']}")
+    print(f"URL: {event['url']}")
+else:
+  print("No Ticketmaster events found")
